2d3c2b4016d8dd49b63a07ecdc9ba87438edc814
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-12-01 11:32:17 -0300

    Implementa: Todos os icons da Sidebar

--- my-app/src/Components/Sidebar.js
+++ my-app/src/Components/Sidebar.js
@@ -2,7 +2,7 @@ import { Center, Grid, Image, Text } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import Pex from "../Images/Pex.jpg";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faCartShopping, faSquarePollVertical, faChartBar } from "@fortawesome/free-solid-svg-icons";
 
 export default function Sidebar() {
   return (
@@ -12,10 +12,18 @@ export default function Sidebar() {
       <FontAwesomeIcon icon={faHouse} />
       <Text>Dashboard</Text>
       </Center>
-    
+      <Center marginRight='34px' gap={5}>
+      <FontAwesomeIcon icon={faCartShopping}  />
       <Text>Vendas</Text>
+      </Center>
+      <Center marginRight='34px' gap={5}>
+      <FontAwesomeIcon icon={faSquarePollVertical} />
       <Text>Relatórios</Text>
+      </Center>
+      <Center marginRight='34px' gap={5}>
+      <FontAwesomeIcon icon={faChartBar} />
       <Text>Lojas Parceiras</Text>
+      </Center>
     </Grid>
   );
 }
