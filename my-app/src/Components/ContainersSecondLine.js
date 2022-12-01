4fd637f29685f03bef9f7f0c134ffc82d396ce22
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-12-01 14:57:45 -0300

    Implementa: Finaliza primeiro container da Segunda Linha

--- my-app/src/Components/ContainersSecondLine.js
+++ my-app/src/Components/ContainersSecondLine.js
@@ -1,10 +1,15 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Text } from "@chakra-ui/react";
 import React from "react";
+import '../Style/ContainersSecondLine.css'
 
 export default function ContainersSecondLine() {
   return (
     <Grid h="95px" templateColumns="repeat(6, 1fr)" gap={4}>
-      <GridItem borderRadius="20px" bg="#FFFFFF" />
+      <GridItem borderRadius="20px" bg="#FFFFFF" >
+        <Text className="text">Clientes Ativos</Text>
+        <Text className="number">3.312</Text>
+        <Text className="numberFinal">+ 2,6% (+12)</Text>
+      </GridItem>
       <GridItem borderRadius="20px" bg="#FFFFFF" />
       <GridItem borderRadius="20px" bg="#FFFFFF" />
       <GridItem borderRadius="20px" bg="#FFFFFF" />
