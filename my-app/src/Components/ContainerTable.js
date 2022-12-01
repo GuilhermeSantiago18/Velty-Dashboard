e92030ba6321ffe013bd332c733d625ed79fd2a6
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-12-01 17:39:32 -0300

    Implementa: Finaliza primeiro container do Footer com tabela

---
+++ my-app/src/Components/ContainerTable.js
@@ -0,0 +1,56 @@
+import {
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import '../Style/ContainerTable.css'
+
+export default function ContainerTable() {
+  return (
+    <>
+    <Text className="first-text-table">Planos</Text>
+    <Text className="second-text-table">123 planos ativos</Text>
+    <TableContainer marginTop='10px'>
+    <Table variant='simple' colorScheme='whiteAlpha'>
+      <Thead className="line-table">
+        <Tr className="line-table">
+          <Th>CLUBE</Th>
+          <Th>CLIENTES</Th>
+          <Th>PREÇO</Th>
+        </Tr>
+      </Thead >
+      
+      <Tbody className='tbody'>
+        <Tr>
+          <Td>Estagiário</Td>
+          <Td>312 Clientes</Td>
+          <Td>123,90</Td>
+        </Tr>
+        <Tr>
+          <Td>Diretoria</Td>
+          <Td>312 Clientes</Td>
+          <Td>123,90</Td>
+        </Tr>
+        <Tr>
+          <Td>Geral</Td>
+          <Td>312 Clientes</Td>
+          <Td>123,90</Td>
+        </Tr>
+        <Tr>
+          <Td>Almoxarifado</Td>
+          <Td>312 Clientes</Td>
+          <Td >123,90</Td>
+        </Tr>
+        </Tbody>
+       
+    </Table>
+  </TableContainer>
+  </>
+  );
+}
--- my-app/src/Components/ContainersFooter.js
+++ my-app/src/Components/ContainersFooter.js
@@ -1,10 +1,13 @@
 import { Grid, GridItem } from '@chakra-ui/react'
 import React from 'react'
+import ContainerTable from './ContainerTable'
 
 export default function ContainersFooter() {
   return (
-    <Grid h="351px" templateColumns="repeat(3, 1fr)" gap={4}>
-    <GridItem borderRadius="20px" bg="#FFFFFF" />
+    <Grid h="370px" templateColumns="repeat(3, 1fr)" gap={4}>
+    <GridItem borderRadius="20px" bg="#FFFFFF" >
+      <ContainerTable />
+    </GridItem>
     <GridItem borderRadius="20px" bg="#FFFFFF" />
     <GridItem borderRadius="20px" bg="#FFFFFF" />
   </Grid>
