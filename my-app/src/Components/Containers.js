e0cc42a454d109701ccea9a4418dca417f5ac31d
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-12-01 14:30:11 -0300

    Implementa: Primeiro Container finalizado

--- my-app/src/Components/Containers.js
+++ my-app/src/Components/Containers.js
@@ -1,17 +1,23 @@
-import { Grid, GridItem, Text } from "@chakra-ui/react";
+import { Grid, GridItem, Text, Image } from "@chakra-ui/react";
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleDollarToSlot } from "@fortawesome/free-solid-svg-icons";
 import '../Style/Containers.css'
+import Shadow from "../Images/Svg/Shadow.svg";
+import Vector from "../Images/Svg/Vector.svg";
 
 export default function Containers() {
   return (
     
     <Grid h="310px" templateColumns="repeat(4, 1fr)" gap={4}>
       <GridItem  borderRadius='20px' colSpan={1} bg='#EC7A7A'>
-      <FontAwesomeIcon icon={faCircleDollarToSlot} width='90px' fontSize='56px' color="#FFFFFF" className="icon-container"/>
+        <div className="containers" borderRadius='50%'>
+      <Image src={Vector} width='16px' marginLeft='20px' position='absolute'></Image>
+        </div>
+        <Text className="text-number-container">R$ 312.321,00</Text>
+        <Text className="text-container">Receita Total</Text>
+        <div className="line-container" />
+        <Text className="second-number">R$ 67,20</Text>
+        <Text className="second-text">Média por cliente</Text>
       </GridItem>
-      
       <GridItem  borderRadius='20px' bg='#7A85EC' />
       <GridItem borderRadius='20px' bg="#65D5AC" />
     </Grid>
