32ddfcc227d32233f5a286c7e78e9a8a4b67a713
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-12-01 15:57:07 -0300

    Implementa: Primeiro container da terceira linha finalizado

--- my-app/src/Components/ContainersThirdLine.js
+++ my-app/src/Components/ContainersThirdLine.js
@@ -1,10 +1,25 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import Vector4 from "../Images/Svg/Vector4.svg";
+import '../Style/ContainersThirdLine.css';
 
 export default function ContainersThirdLine() {
   return (
     <Grid h="196px" templateColumns="repeat(4, 1fr)" gap={4}>
-    <GridItem borderRadius="20px" bg="#FFFFFF" />
+    <GridItem borderRadius="20px" bg="#FFFFFF">
+        <div className="containers-third" borderRadius='50%'>
+          <Image
+            src={Vector4}
+            width="16px"
+            marginLeft="20px"
+            position="absolute"
+            marginTop='13px'
+          ></Image>
+        </div>
+        <Text className="text-number-container-third">R$ 312.321</Text>
+        <Text className="text-container-third">Cotações Realizadas</Text>
+        <Text className="number-final-third">+ 15,3% (+223)</Text>
+        </GridItem>
     <GridItem borderRadius="20px" bg="#FFFFFF" />
     <GridItem borderRadius="20px" bg="#FFFFFF" />
     <GridItem borderRadius="20px" bg="#FFFFFF" />
